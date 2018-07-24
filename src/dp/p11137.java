114413f65544eb023e23915bd12bab0df3490558
sourav7 <souravdebnath7@gmail.com>
2018-07-24 01:31:46 -1200

    Update p11137.java

--- src/dp/p11137.java
+++ src/dp/p11137.java
@@ -20,11 +20,11 @@ public class Problem11137 {
 			int n = reader.nextInteger();
 			System.out.println(cubrency.getNumberOfWays(n));
 		}
+
 	}
 }
 
 class IngenuousCubrency {
-	private int coins[];
 
 	private final int MAX_LIMIT = 10002;
 	private int coinIndexLimit = 0;
@@ -32,8 +32,7 @@ class IngenuousCubrency {
 
 	public IngenuousCubrency(int numberOfCoins) {
 		this.coinIndexLimit = numberOfCoins + 1;
-		resultArray = new long[MAX_LIMIT];
-		this.makeCoins();
+		this.resultArray = new long[MAX_LIMIT];
 		this.generateResult();
 	}
 
@@ -44,21 +43,13 @@ class IngenuousCubrency {
 	private void generateResult() {
 		resultArray[0] = 1;
 		for (int i = 1; i < coinIndexLimit; i++) {
-			int currentCoin = coins[i];
+			int currentCoin = i*i*i;
 			int start = currentCoin;
 
 			while (start < MAX_LIMIT) {
 				resultArray[start] = resultArray[start] + resultArray[start - currentCoin];
 				start++;
 			}
-		}
-	}
-
-	private void makeCoins() {
-		coins = new int[this.coinIndexLimit];
-		coins[0] = 0;
-		for (int i = 1; i < this.coinIndexLimit; i++) {
-			coins[i] = i * i * i;
 		}
 	}
 }
